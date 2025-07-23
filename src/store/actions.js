12d7b0e2a8964c7503fc9b0eebf9c1d06053da66
Shrutiyadv <shruti.yadav@univolenitsolutions.com>
2025-07-23 19:55:52 +0530

    Add Tour Group Variants table UI

--- src/store/actions.js
+++ src/store/actions.js
@@ -6,7 +6,7 @@ export * from "./auth/login/actions";
 export * from "./auth/forgetpwd/actions";
 export * from "./auth/profile/actions";
 
-//Ecommerce
+
 export * from "./e-commerce/actions";
 
 //Calendar
